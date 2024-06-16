060334bff7b9090e272633c42cc21f2a16889010
frozar <fabien.rozar@gmail.com>
2024-06-17 02:18:28 +0400

    Refactoring. Display tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,54 @@ import { onMount, type Component } from "solid-js";
 
 import styles from "./App.module.css";
 import "leaflet/dist/leaflet.css";
-import { map, latLng, tileLayer, MapOptions, geoJSON } from "leaflet";
+import { map, latLng, tileLayer, MapOptions, geoJSON, Layer } from "leaflet";
+import { GeoJsonObject } from "geojson";
+
+async function initialiseMap() {
+  const options: MapOptions = {
+    center: latLng(-21.14695277642929, 55.53039550781251),
+    zoom: 9,
+  };
+
+  const mymap = map("map", options);
+
+  tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+  }).addTo(mymap);
+
+  const geoJson = await (
+    await fetch("/circonscriptions_legislatives_030522.json")
+  ).json();
+
+  const myStyle = {
+    color: "#ff7800",
+    weight: 5,
+    opacity: 0.45,
+  };
+
+  const geoJsonLayer = geoJSON(geoJson.features as GeoJsonObject[], {
+    style: myStyle,
+  });
+
+  geoJsonLayer.bindPopup(function (layer) {
+    return (
+      <a href="http://www.google.com" target="_blank">
+        {layer.feature.properties.libelle}
+      </a>
+    );
+  });
+
+  geoJsonLayer.addTo(mymap);
+
+  return mymap;
+}
 
 const App: Component = () => {
   onMount(async () => {
-    console.log("On mount");
-
-    const options: MapOptions = {
-      center: latLng(-21.14695277642929, 55.53039550781251),
-      zoom: 11,
-    };
-
-    const mymap = map("map", options);
+    const mymap = await initialiseMap();
 
     window.mymap = mymap;
-
-    tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
-    }).addTo(mymap);
-
-    const geoJson = await (
-      await fetch("/circonscriptions_legislatives_030522.json")
-    ).json();
-
-    console.log(geoJson);
-
-    const myStyle = {
-      color: "#ff7800",
-      weight: 5,
-      opacity: 0.45,
-    };
-
-    geoJSON(geoJson.features, {
-      style: myStyle,
-    }).addTo(mymap);
   });
 
   return <div id="map" class={styles.map}></div>;
